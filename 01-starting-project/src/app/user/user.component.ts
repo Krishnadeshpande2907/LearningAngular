e13e3d7502a263255a7b36f918f19f9e3a241393
Krishna Deshpande <kd721921@gmail.com>
2025-11-11 23:33:19 +0530

    day 3 till signals

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,9 +11,22 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex]
+  // with Input decorator
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+
+  // do not use this method in a lot of places
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imgPath = computed(() => {
+  //   'assets/users/' + this.avatar
+  // });
+  // with computed, the imgPath will only be recalculated when this.avatar changes
 
   get imgPath() {
-    return 'assets/users/' + this.selectedUser.avatar;
+    return 'assets/users/' + this.avatar;
   }
+  
+  onSelectUser() {  }
 }
