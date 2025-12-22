070a4eaa67002f0b8f0f5bf575a7e2c22d95c106
Krishna Deshpande <kd721921@gmail.com>
2025-12-23 01:32:46 +0530

    component deep dive done

---
+++ custom-2w-binding-project/src/app/rect/rect.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, model, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-rect',
+  standalone: true,
+  imports: [],
+  templateUrl: './rect.component.html',
+  styleUrl: './rect.component.css',
+})
+export class RectComponent {
+  // Todo: Implement custom two-way binding
+  // @Input({required: true}) size!: {width: string; height: string};
+  // @Output() sizeChange = new EventEmitter<{width: string; height: string}>();
+
+  // Using signals for two-way binding
+  size = model.required<{width: string; height: string }>();
+
+  onReset() {
+    this.size.set({
+      width: '100', 
+      height: '100'
+    });
+  }
+}
