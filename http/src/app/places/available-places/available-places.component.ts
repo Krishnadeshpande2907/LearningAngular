32f7a3b45c1a4efd58135a5a9bffa43be1a6bc45
Krishna Deshpande <kd721921@gmail.com>
2026-01-14 00:26:02 +0530

    learning http requests stuff

---
+++ http/src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,39 @@
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyRef= inject(DestroyRef);
+
+  // also possible if used constructors
+  // constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    const subscription = this.httpClient
+    .get<{places: Place[]}>('http://localhost:3000/places')
+    .subscribe({
+      next: (resData) => {
+        console.log(resData.places);
+      }
+    });
+
+    // technically not required but good practice
+    this.destroyRef.onDestroy(() => {
+      // cleanup logic here
+      subscription.unsubscribe();
+    });
+  }
+
+}
